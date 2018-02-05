1fbeb615e6d73c4232589fa23a7fdb7a519e02f8
Gustavo Almeida <gustatec@outlook.com>
2018-02-05 03:46:21 -0200

    fix

--- src/app/movies/movie-service.service.ts
+++ src/app/movies/movie-service.service.ts
@@ -7,6 +7,7 @@ import { MovieDetail } from './models/movie-detail.model';
 @Injectable()
 export class MovieService {
   private urlBase = 'https://api.themoviedb.org/3/';
+  //colocar no config.json
   private apiKey = '0db8a6622b8920b82816a4171cfe5901';
 
   constructor(private http: HttpClient) {
@@ -27,6 +28,7 @@ export class MovieService {
 
   private getApiUrl(method: string, params: any = {}) {
     var url = this.urlBase + method + "?api_key=" + this.apiKey + "&language=pt-BR&append_to_response=videos,images,keywords,recommendations,credits,collection";
+    
     Object.keys(params).forEach(function (key, index) {
       url += "&" + key + "=" + params[key];
     });
